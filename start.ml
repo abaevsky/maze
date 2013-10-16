d9899007ed1f8ca1d21fef3ba4dd9ef6c07b1eb6
abaevsky <dir337@yandex.ru>
2013-10-16 11:48:40 +0600

    Прикрутил функцию вывода массива

--- start.ml
+++ start.ml
@@ -1,3 +1,16 @@
+let show k = 
+	for i = 0 to (Array.length k) -1 do
+		let string = k.(i) in
+			begin
+			for i = 0 to (Array.length string) -1 do
+				print_char (' ');
+				print_int (string.(i));
+				done
+			end;
+		print_newline();
+	done;;
+			
+
 let ways = 
 let num = Array.length (Sys.argv) in
 	begin
@@ -6,6 +19,10 @@ let num = Array.length (Sys.argv) in
 	else
 	Array.make_matrix (int_of_string Sys.argv.(1)) (int_of_string Sys.argv.(2)) 0
 	end;;
-
-print_int (Array.length ways);;
+ways.(0).(0) <- 5;;
+ways.(3).(2) <- 8;;
+print_int (ways.(0).(0));;
+print_char ('X');
+print_newline ();;
+show ways;;
 print_newline ();;
