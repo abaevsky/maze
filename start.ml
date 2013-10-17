7e5afe3970a237ca40e2bf4d148421993f1639b1
abaevsky <dir337@yandex.ru>
2013-10-17 11:24:04 +0600

    Глубоко в отладке. Проблемы у меня

--- start.ml
+++ start.ml
@@ -16,9 +16,7 @@ let show prm =
 			print_int k2)
 	 k; print_newline()) prm;;	
 	 
-*)
-	
-	
+*)	
 
 let show filed vw hw =	
 	for i = 0 to (Array.length filed) -1 do
@@ -30,9 +28,7 @@ let show filed vw hw =
 			Array.iter (fun k -> print_char (' '); 
 				if k then print_char ('-') else print_char (' ')) hw.(i);
 			print_newline ();
-	done;;
-			
-			
+	done;;		
 
 let ways = 
 let num = Array.length (Sys.argv) in
@@ -58,45 +54,105 @@ let rec track filed door outcome =
 		match { filed.(door.(0)).(door.(1)), filed.(outcome.(0)).(outcome.(1))} with
 		{ 2, _} || { _, 1} 
 *)	
-	
-let resetmaze () = 
-let numy = Array.length ways.(0) and numx = Array.length ways in 
-	Array.fill ways 0  numx (Array.make numy 0);;	
-	
-	
-ways.(0).(0) <- 5;;
-ways.(3).(2) <- 8;;
-print_int (ways.(0).(0));;
-print_char ('X');
+
+let numy = Array.length ways.(0) and numx = Array.length ways;;
+
+print_int (numx);;
+print_char (' ');;
+print_int (numy);;
 print_newline ();;
 
 (*
+let sumgam filed v1 v2 =
+	let way2 = filed  in 
+	for i = 0 to (numx - 1) do
+		for j = 0 to (numy -1) do 
+			way2.(i).(j) <- (
+				(if (i > 0)  then (if (v1.(i).(j)) then (filed.(i - 1).(j)) else 32) else 2) +
+				(if (i < (numx - 1))  then (if (v1.(i + 1).(j)) then filed.(i + 1).(j) else 64) else 4) +
+				(if (j > 0)  then (if (v2.(i).(j-1)) then (filed.(i).(j - 1)) else 128) else 8) +
+				(if (j < (numy - 1))  then (if (v2.(i).(j)) then filed.(i).(j + 1) else 256) else 16)
+			)
+		done
+	done;
+	way2;;
+	*)
+	
+let sumgam filed v1 v2 =
+	let way2 = filed  in 
+	for i = 0 to (numx - 1) do
+		for j = 0 to (numy -1) do 
+			way2.(i).(j) <- (
+				(if (i > 0)  then  32 else 2) +
+				(if (i < (numx - 1))  then  64 else 4) +
+				(if (j > 0)  then 128 else 8) +
+				(if (j < (numy - 1))  then  256 else 16)
+			)
+		done
+	done;
+	way2;;
+	
+	
+let resetmaze () = 
+	Array.fill ways 0  numx (Array.make numy 1);;	
 
+let randwall () =
+	for i = 0 to (numx -1) do
+	for j = 0 to (numy -1) do vwall.(i).(j) <- Random.bool ();  hwall.(i).(j) <- Random.bool () done
+	done;;
+
+
+	
+	
+(*
 let
 	v1 = Array.map (Array.map (fun x ->  Random.bool ())) vwall
 and
 	v2 = Array.map (Array.map (fun x ->  Random.bool ())) hwall
 in
+(*
 Переменные для тестов вывода стенок
 *)
 
+show ways v1 v2;;
+*)
+for i = 0 to (numx -1) do
+	for j = 0 to (numy -1) do ways.(i).(j) <- (i + j * 32) done
+	done;;
+
 show ways vwall hwall;;
 print_newline ();;
-
 
 Random.init;;
-for i = 0 to (Array.length ways - 1) do
-	for j = 0 to (Array.length ways.(0) -1) do ways.(i).(j) <- Random.int 7 done
+for i = 0 to (numx -1) do
+	for j = 0 to (numy -1) do ways.(i).(j) <- Random.int 2 done
 	done;;
 	
-	
-	
 
+		
 show ways vwall hwall;;
 print_newline ();;
 
+
 resetmaze ();;
-show ways vwall hwall;;
+(*
+letvwall.(4).(2) <- true;; 
+	wall = Array.make_matrix numx numy true
+in
+show (sumgam
+*)
+
+(*
+randwall ();;*)
+(*(sumgam ())*)
+Random.init;;
+(*
+hwall.(1).(2) <- true;; 
+hwall.(2).(2) <- true;; 
+vwall.(2).(2) <- true;; 
+vwall.(2).(3) <- true;; 
+*)
+show  (sumgam ways vwall hwall) vwall hwall;;
 print_newline ();;
 
 
